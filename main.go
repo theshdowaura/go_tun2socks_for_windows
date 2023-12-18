eb74fd00b466c7dd98a8fface06aa1603b270bc6
theshdowaura <haojiejack@gmail.com>
2023-12-18 21:24:40 +0800

    update:remove Error handling

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"go_tun2socks_for_windows/gui"
-	"log"
 )
 
 func main() {
-	if err := gui.RunMainWindow(); err != nil {
-		log.Println("启动出错", err)
-	}
+	gui.Runmainwindows()
 }
